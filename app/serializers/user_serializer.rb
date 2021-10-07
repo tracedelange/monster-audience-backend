515b86787ef4d43f3f1b2a6e4a14f47edb69ad25
Trace <tracedelange@me.com>
2021-10-07 16:00:14 -0500

    added some stats

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -9,7 +9,13 @@ class UserSerializer < ActiveModel::Serializer
     following_count = @user.friends.count
     follower_count = Friendship.where(friend_id: @user.id).count
 
-    {"following" => following_count, "followers" => follower_count}
+    review_count = Review.where(user_id: @user.id).count
+    subject_count = Subject.where(user_id: @user.id).count
+
+    avg = Review.where(user_id: @user.id)
+    
+
+    {"following" => following_count, "followers" => follower_count, "reviews" => review_count, "subjects" => subject_count, "avg_rating" => avg.average('rating')}
 
   end
 
