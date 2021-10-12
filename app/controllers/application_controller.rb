78ec8b385596b46783ec4bcc502863e83d1e2380
Trace <tracedelange@me.com>
2021-10-12 17:12:59 -0500

    db creds updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
 
     def encode_token(payload)
       # should store secret in env variable
-      JWT.encode(payload, ENV['DEV_HASH_KEY'])
+      JWT.encode(payload, ENV['HASH_KEY'])
     end
   
     def auth_header
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::API
         token = auth_header.split(' ')[1]
         # header: { 'Authorization': 'Bearer <token>' }
         begin
-          JWT.decode(token, ENV['DEV_HASH_KEY'], true, algorithm: 'HS256')
+          JWT.decode(token, ENV['HASH_KEY'], true, algorithm: 'HS256')
         rescue JWT::DecodeError
           nil
         end
